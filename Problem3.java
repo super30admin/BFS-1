0d4fe5fe9bfe34c70142eaa703a12681b9f1c702
Ashwin Saxena <ashwin@Ashwins-MacBook-Air.local>
2019-11-05 20:55:17 -0500

    Completed Binary Tree Right Side View

---
+++ Problem3.java
@@ -0,0 +1,59 @@
+/** Binary Tree Right Side View
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <--- */
+
+  // Time Complexity : O(n)
+// Space Complexity : O(n) as bfs visits tree layer by layer
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<Integer, Integer>();
+        int max_depth = -1;
+
+        //These two Queues are always synchronized, providing an implicit association values with the same offset on each Queue. 
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<Integer> depthQueue = new LinkedList<Integer>();
+        nodeQueue.add(root);
+        depthQueue.add(0);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+            int depth = depthQueue.remove();
+
+            if (node != null) {
+                max_depth = Math.max(max_depth, depth);
+
+                //The last node that we encounter at a particular depth contains
+                //the correct value, so the correct value is never overwritten. 
+                rightmostValueAtDepth.put(depth, node.val);
+                nodeQueue.add(node.left);
+                nodeQueue.add(node.right);
+                depthQueue.add(depth+1);
+                depthQueue.add(depth+1);
+            }
+        }
+
+        //Construct the solution based on the values that we end up with at the end
+        List<Integer> rightView = new ArrayList<Integer>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+        return rightView;
+    }
+}
