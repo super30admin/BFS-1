d936c906fa10da8dd9851de45d23110548b517c6
88sanjay <88.sanjay@gmail.com>
2019-11-05 22:31:33 -0500

    Add docstring

--- course_scheduling_bfs.py
+++ course_scheduling_bfs.py
@@ -1,3 +1,12 @@
+"""
+Compute the indegree of all course prereqs. 
+Remove edges of nodes with 0 indegree from the prereqs edge list.
+Continue this process till no node with 0 indegree exists. If edge list is empty then 
+return true else return false
+Time complexity =  O(E +V)
+Space complexity = O(E+V)
+
+"""
 class Solution:
     
     def remove(self,prereqs,v) :
