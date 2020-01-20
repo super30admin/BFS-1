b249eefd47e3040717ffc8ac974059cff59aa90d
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-20 15:42:39 -0500

    binary tree right side view

--- 199_binary_tree_right_side_view.py
+++ 199_binary_tree_right_side_view.py
@@ -4,15 +4,37 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
+
 
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
         """
             https://leetcode.com/problems/binary-tree-right-side-view/
-            // Time Complexity :
-            // Space Complexity :
-            // Did this code successfully run on Leetcode :
-            // Any problem you faced while coding this :
+            // Time Complexity : O(n)
+                'n' is the number of nodes
+            // Space Complexity : O(n)
+            // Did this code successfully run on Leetcode : Yes
+            // Any problem you faced while coding this : No
             // Three line explanation of solution in plain english :
+                    - Level order traversal
+                    - For each level in the tree add the right most element
         """
-        pass
+        result = []
+        if not root:
+            return result
+        # level order traversal
+        queue = deque()
+        queue.append(root)
+        while queue:
+            cur_size = len(queue)
+            while cur_size > 0:
+                cur = queue.popleft()
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+                if cur_size == 1:
+                    result.append(cur.val)
+                cur_size -= 1
+        return result
