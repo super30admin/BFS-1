510b4c9c10aacb4f565e48501c4551daab832d47
Chaitrali Deshmukh <42701774+ChaiDeshmukh@users.noreply.github.com>
2020-03-02 16:24:51 -0800

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,19 @@
+Binary Tree Level Order Traversal
+
+
+Approach 1: BFS
+
+create an empty queue
+add the root in a queue
+Loop while queue is not empty
+	-> iterate through the size of this queue
+ -> poll the first element from queue into temp
+ -> add it to array list
+ -> add its left & right child to queue
+ 
+ 
+ Time Complexity: O(n)
+ Space Complexity: O(n)
+ 
+ 
+ 
