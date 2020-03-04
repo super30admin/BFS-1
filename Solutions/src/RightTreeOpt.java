8a796926ed799afdb92b953ebb8f03ecce7cf48f
rohank2002 <rohansantosh.kamat@sjsu.edu>
2020-03-03 17:20:18 -0800

    Completed

--- Solutions/src/RightTreeOpt.java
+++ Solutions/src/RightTreeOpt.java
@@ -1,4 +1,4 @@
-// Time Complexity :O(n^2)
+// Time Complexity :O(n)
 // Space Complexity :O(1)
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this : No
@@ -11,11 +11,11 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class RightTree {
+class RightTreeOpt {
     public List<Integer> rightSideView(TreeNode root) {
 
 
-        List <TreeNode> l = new LinkedList<TreeNode>();
+        Queue <TreeNode> q = new LinkedList<TreeNode>();
         List<Integer> res = new LinkedList<Integer>();
         if (root==null){
             return res;
@@ -24,29 +24,24 @@ class RightTree {
             res.add(root.val);
             return res;
         }
-        res.add(root.val);
-        l.add(root);
-        while(!l.isEmpty()){
-            int level = l.size();
-
-            for(int i = 0;i<level;i++){
-                TreeNode current = l.get(0);
+        q.add(root);
+        while(!q.isEmpty()){
+            int countNodes = q.size();
+            for(int i = 0;i<countNodes;i++){
+                TreeNode current = q.poll();
+                if(i==countNodes-1){
+                    res.add(current.val);
+                }
                 if(current.left!=null){
-                    l.add(current.left);
+                    q.add(current.left);
                 }
                 if(current.right!=null){
-                    l.add(current.right);
+                    q.add(current.right);
                 }
-                if(i == level -1){
-                    TreeNode last = l.get(l.size()-1);
-                    res.add(last.val);
-                }
-                l.remove(0);
-
 
             }
+
         }
-        res.remove(res.size()-1);
         return res;
     }
 }
