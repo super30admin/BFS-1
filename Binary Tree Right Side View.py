b18ed04fe9291d5711602712f493c38fe079c7f8
Sowndhariya Nandarajkumar <41890348+SoundNandu@users.noreply.github.com>
2020-03-05 00:26:02 -0800

    Update Binary Tree Right Side View.py

--- Binary Tree Right Side View.py
+++ Binary Tree Right Side View.py
@@ -4,6 +4,11 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+
+#BFS
+#----------------------------------------
+
 from collections import deque
 class Solution:
     
@@ -25,3 +30,35 @@ class Solution:
                     queue.append(currrent.right)
             result.append(last)
         return result
+
+    
+    
+    
+    
+    
+ #DFS
+#--------------------------------
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        self.result = []
+        if self.result == None:
+            return self.result
+        self.helper(root, 0)
+        return self.result
+            
+    def helper(self, root, level):
+      
+        if(not root):
+            return
+        if(level == len(self.result)):
+            self.result.append(root.val)
+        self.helper(root.right, level + 1)
+        self.helper(root.left, level + 1)
+       
+
