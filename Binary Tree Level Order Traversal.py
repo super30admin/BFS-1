e9b70abffc2103cdb92c536640e090467e7f8460
Sowndhariya Nandarajkumar <41890348+SoundNandu@users.noreply.github.com>
2020-03-04 16:35:02 -0800

    Update Binary Tree Level Order Traversal.py

--- Binary Tree Level Order Traversal.py
+++ Binary Tree Level Order Traversal.py
@@ -1,1 +1,29 @@
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+class Solution:
+
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root == None:
+            return []
+        result = []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            temp = []
+            for _ in range(size):
+                currrent = queue.popleft()
+                temp.append(currrent.val)
+                if currrent.left:
+                    queue.append(currrent.left)
+                if currrent.right:
+                    queue.append(currrent.right)
+            result.append(temp)
+        return result
