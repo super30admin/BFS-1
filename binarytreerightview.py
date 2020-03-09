94a15d810b7c6422ba405bd39b1dfbd576a36509
Pratiksha Prabhu <prabhup@usc.edu>
2020-03-09 13:50:42 -0700

    Completed all problems

--- binarytreerightview.py
+++ binarytreerightview.py
@@ -3,22 +3,18 @@
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
-
 # Your code here along with comments explaining your approach
 
-from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+from collections import deque
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        
+    def rightSideView(self, root: TreeNode) -> List[int]:
         result = []
-        
         if root == None:
             return result
         
@@ -27,32 +23,19 @@ class Solution:
         
         while queue:
             count = len(queue)
-            levelelements = []
             
             for i in range(count):
                 element = queue.popleft()
-                levelelements.append(element.val)
-                
+                if i == count-1:
+                    result.append(element.val)
                 if element.left!=None:
                     queue.append(element.left)
-                
+                    
                 if element.right!=None:
                     queue.append(element.right)
-            
-            result.append(levelelements)
-            
-        return result
-           
                     
-                
-                
-            
-                
-            
-                
-                
-                
-                
+        return result
+        
         
         
         
