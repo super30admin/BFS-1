93cc6386462439b705866cf0555bddd97d71c3d9
gayathryR <gayathry.rpillai@gmail.com>
2020-04-14 15:18:19 -0700

    Completed Problem 3

---
+++ Problem3.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Time Complexity=O(n)
+// Space Complexity=O(n)
+// succesfully run on leetcode : Yes
+//problems faced:none
+    
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        if(root==null){
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i<size;i++){
+                TreeNode temp = queue.poll();
+                if(i==size-1){
+                    result.add(temp.val);
+                }
+                if(temp.left!=null){
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null){
+                    queue.offer(temp.right);
+                }
+            }
+        }
+        
+        return result;
+        
+    }
+}
