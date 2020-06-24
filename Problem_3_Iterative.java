d4cde068fc5781fed0f0bcd05c57fe257d97abb8
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-23 18:09:44 -0700

    Create Problem_3_Iterative.java

---
+++ Problem_3_Iterative.java
@@ -0,0 +1,41 @@
+// Time complexity - O(n)
+// Space complexity - O(n)
+
+// Do level order traversal and store rightmost element.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int tmp = 0;
+            for(int i=0; i< size; i++){
+                TreeNode curr = q.poll();
+                tmp = curr.val;
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+            result.add(tmp);
+        }
+        return result;
+    }
+}
