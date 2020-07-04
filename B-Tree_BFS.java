ea4e9a54c669fa42e37a22fb9425833dec317905
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-07-04 14:02:09 -0400

    renamed files

--- B-Tree_BFS.java
+++ B-Tree_BFS.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n) --> where n is number of nodes in the tree  
-// Space Complexity : O(h) --> where h is height of the tree
+// Space Complexity : O(n/2)
 // Did this code successfully run on Leetcode (102): Yes
 // Any problem you faced while coding this : No
 
@@ -7,27 +7,25 @@
 // Your code here along with comments explaining your approach
 
 class Solution {
-    // DFS Approach
-    List<List<Integer>> result;
     public List<List<Integer>> levelOrder(TreeNode root) {
-        result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
-        dfs(root, 0);
-        return result;
-    }
-    
-    private void dfs(TreeNode root, int level) {
-        // base case
-        if (root == null) return;
         
-        // logic
-        // check if I need to add arraylist corresponding to the current level of my node
-        if (result.size() == level) {
-            result.add(new ArrayList<>());
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> temp = new ArrayList<>();
+            // for level process
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                temp.add(curr.val);
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+            }
+            result.add(temp);
         }
-        result.get(level).add(root.val);
-        
-        dfs(root.left, level + 1);
-        dfs(root.right, level + 1);
+        return result;
     }
 }
--- B-Tree_DFS.java
+++ B-Tree_DFS.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n) --> where n is number of nodes in the tree  
-// Space Complexity : O(n/2)
+// Space Complexity : O(h) --> where h is height of the tree
 // Did this code successfully run on Leetcode (102): Yes
 // Any problem you faced while coding this : No
 
@@ -7,25 +7,27 @@
 // Your code here along with comments explaining your approach
 
 class Solution {
+    // DFS Approach
+    List<List<Integer>> result;
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+        result = new ArrayList<>();
         if (root == null) return result;
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> temp = new ArrayList<>();
-            // for level process
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                temp.add(curr.val);
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
-            }
-            result.add(temp);
-        }
+        dfs(root, 0);
         return result;
+    }
+    
+    private void dfs(TreeNode root, int level) {
+        // base case
+        if (root == null) return;
+        
+        // logic
+        // check if I need to add arraylist corresponding to the current level of my node
+        if (result.size() == level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
     }
 }
