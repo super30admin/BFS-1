acccaa633a47a2889c1a82ee951a33c34eaca991
Lavanya <lavanya.ramaswamy@sjsu.edu>
2020-08-05 14:58:18 -0700

    Completed the PR

---
+++ rightSideView.java
@@ -0,0 +1,26 @@
+/* Using BFS
+Time complexity: O(n)
+space complexity: O(n/2) = O(n)
+*/
+
+class Solution{
+    public List<Integer> rightSideView(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                TreeNode curr = q.poll();
+                if(i == size-1){
+                    result.add(curr.val);
+                }
+            }
+            if(curr.left != null) q.add(curr.left);
+            if(curr.right != null) q.add(curr.right);
+        }
+
+        return result;
+    }
+}
