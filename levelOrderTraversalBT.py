2be75d31aacb762ca817f0e9c31ff8df13718434
vatsaashwin <aashwinvats@GMAIL.com>
2020-09-15 16:13:46 -0700

    Completed Course Schedule and level order iterative

--- levelOrderTraversalBT.py
+++ levelOrderTraversalBT.py
@@ -42,6 +42,50 @@ class Solution:
             self.recur(root.left, level+1)
         if root.right:
             self.recur(root.right, level+1)
-            
+
+#----------------------------------------------------------------                  
+# // Time Complexity : O(V+E)
+# // Space Complexity : O(diameter of the tree)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+
+# // Your code here along with comments explaining your approach: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#Iterative approach, discussed in class
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        
+        output = []
+        
+        if not root:
+            return output
+
+        # initialise queue with the root     
+        queue = [root]
+    
+        # while queue is not empty maintain size of queue at all levels and keep adding all nodes in the queue level wise
+        # if popped node's left or right exist: add them to the queue in that order
+        while queue:
+            size = len(queue)
+            temp = []
+            for x in range(size):
+                front = queue.pop(0)
+                temp.append(front.val)
+                if front.left is not None:
+                    queue.append(front.left)
+                if front.right is not None:
+                    queue.append(front.right)
+                    
+            output.append(temp)
+        
+        return output
+    
         
         
