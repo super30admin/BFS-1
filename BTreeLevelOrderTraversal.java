ceeb4446911ea1b38ca59740c136516fee7f542d
Arpit Sharma <arpit317@gmail.com>
2020-10-02 15:56:33 -0700

    Update BTreeLevelOrderTraversal.java

--- BTreeLevelOrderTraversal.java
+++ BTreeLevelOrderTraversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+//BFS
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList();
@@ -33,5 +34,25 @@ class Solution {
             result.add(temp);
         }
         return result;
+    }
+}
+
+//DFS
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        result = new ArrayList();
+        if(root == null) return result;
+        dfs(root, 0);
+        return result;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root == null) return;
+        if(level == result.size()){
+            result.add(new ArrayList());
+        }
+        result.get(level).add(root.val);
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
     }
 }
