ae869c8eacaf13b3ef57e169f20fede3aae1e48b
Arpit Sharma <arpit317@gmail.com>
2020-10-02 23:24:26 -0700

    Update BinaryTreeRightSideView.java

--- BinaryTreeRightSideView.java
+++ BinaryTreeRightSideView.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+//DFS
 class Solution {
     List<Integer> result;
     public List<Integer> rightSideView(TreeNode root) {
@@ -32,7 +33,7 @@ class Solution {
     }
 }
 
-
+//DFS
 class Solution {
     List<Integer> result;
     public List<Integer> rightSideView(TreeNode root) {
@@ -53,5 +54,26 @@ class Solution {
         }
         dfs(root.left, level+1);
         dfs(root.right, level+1);
+    }
+}
+
+//BFS
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        if(root == null) return result;
+        
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size(); TreeNode curr = null;
+            for(int i = 0; i < size; i++){
+                curr = q.poll();
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+            result.add(curr.val);
+        }
+        return result;
     }
 }
