6255c37f532509b907da9fe39253b09767d7130b
Arpit Sharma <arpit317@gmail.com>
2020-10-02 23:13:33 -0700

    Update BinaryTreeRightSideView.java

--- BinaryTreeRightSideView.java
+++ BinaryTreeRightSideView.java
@@ -31,3 +31,27 @@ class Solution {
         dfs(root.left, level+1);
     }
 }
+
+
+class Solution {
+    List<Integer> result;
+    public List<Integer> rightSideView(TreeNode root) {
+        result = new ArrayList();
+        if(root == null) return result;
+        dfs(root, 0);
+        return result;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root == null) return;
+        
+        if(level == result.size()){
+            result.add(root.val);
+        }
+        else{
+            result.add(level, root.val);
+            result.remove(level+1);
+        }
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
+    }
+}
