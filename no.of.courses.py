88e06fd46471db37a8a868f84545b07b86ea5fbd
Jaisurya Nanduri <jaisunanduri1@gmail.com>
2020-10-12 20:14:13 -0500

    Finshed BFS-1

--- no.of.courses.py
+++ no.of.courses.py
@@ -1,5 +1,5 @@
-# // Time Complexity : O(N)
-# // Space Complexity : O(1)
+# // Time Complexity : O(V+E)
+# // Space Complexity : O(V)
 # // Did this code successfully run on Leetcode : yes
 # // Any problem you faced while coding this : no
 class Solution:
