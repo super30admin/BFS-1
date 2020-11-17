a398066bf7c5f7026700ace8e89f5ffed2f78df8
Divya Bhat <divyabhat09@gmail.com>
2020-11-16 23:17:37 -0800

    problem-3 completed

---
+++ RightSideView.java
@@ -0,0 +1,23 @@
+//Time Comlexiity O(n)
+//Space - O(H) - HEIGHT OF THE TREE
+
+class Solution {
+    List<Integer> rightside = new ArrayList();
+    
+    public void helper(TreeNode node, int level) {
+        if (level == rightside.size()) 
+            rightside.add(node.val);
+        
+        if (node.right != null) 
+            helper(node.right, level + 1);  
+        if (node.left != null) 
+            helper(node.left, level + 1);
+    }    
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return rightside;
+        
+        helper(root, 0);
+        return rightside;
+    }
+}
