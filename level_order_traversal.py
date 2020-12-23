937edf2ace9582eaad9fe5acedfad62d414df3ff
apurva <apurva@compgeom.com>
2020-12-23 15:38:49 +0530

    completed assignment

---
+++ level_order_traversal.py
@@ -0,0 +1,23 @@
+'''
+TC&SC:O(N)
+'''
+class Solution:
+
+    def levelOrder(self, root):
+        levels = []
+        if not root:
+            return levels
+        
+        def helper(node, level):
+            if len(levels) == level:
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+            
+        helper(root, 0)
+        return levels
