0a42f72278b6d17719239c8ff2f406b51a322120
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2021-01-27 20:46:03 +0530

    Update LevelOrderTraversal.java

--- LevelOrderTraversal.java
+++ LevelOrderTraversal.java
@@ -22,3 +22,26 @@ class Solution {
         return result;
     }
 }
+
+//-------------
+
+//TC - O(n) where n is number od nodes.
+//SC - O(
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        result = new ArrayList<>();
+        if(root == null) return result;
+        dfs(root,0);
+        return result;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root == null) return;
+        if(level == result.size()){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
+    }
+}
