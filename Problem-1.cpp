cc44d73d09581e9e951c782a8889543336408bcd
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-20 18:47:46 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -10,6 +10,7 @@
  * };
  Time complexity = O(N)
  Space complexity = O(N)
+ Where N is the number of nodes in the tree and h is the height of the tree.
  */
 class Solution {
 public:
@@ -36,5 +37,50 @@ public:
             result.push_back(ans);
         }
         return result;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+  Time complexity = O(N)
+ Space complexity = O(h)
+ Where N is the number of nodes in the tree and h is the height of the tree.
+ */
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root==NULL)
+            return result;
+        dfs(root,0);
+        return result;
+    }
+    void dfs(TreeNode *root, int lvl)
+    {
+        //base
+        if(root==NULL)
+            return;
+        //logic
+        if(result.size()==lvl)
+        {
+            vector<int> ans;
+            ans.push_back(root->val);
+            result.push_back(ans);
+        }
+        else
+        {
+            result[lvl].push_back(root->val);
+        }
+        dfs(root->left,lvl+1);
+        dfs(root->right,lvl+1);
     }
 };
