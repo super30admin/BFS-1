32ef0b1b155a228d5b24bcb58e3c1f96b57fcb60
Adhish Shrivastava <adhish70@gmail.com>
2021-11-01 21:26:00 -0400

    Initial Commit

---
+++ Problem-56 BT Level Order Traversal.py
@@ -0,0 +1,18 @@
+# 102. Binary Tree Level Order Traversal
+# https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+# Logic: 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
+
---
+++ Problem-57 Course Schedule.py
@@ -0,0 +1,11 @@
+# 207. Course Schedule
+# https://leetcode.com/problems/course-schedule/
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+# Logic: 
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pass
