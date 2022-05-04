a32a307351e2bf31ecebe53eebac638e3a0f05df
Meena-hash <meena.bhat1995@gmail.com>
2022-05-04 15:25:36 -0700

    completed BFS 1. Please review

---
+++ CourseSchedule.py
@@ -0,0 +1,46 @@
+# // Time Complexity : O(V + E)
+# // Space Complexity : O(V + E)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        #   adjacency list
+        #   indegree
+        adj = {}
+        degree = {}
+
+        for course in range(numCourses):
+            degree[course] = 0
+
+        for course, prereq in prerequisites:
+            degree[course] += 1
+            if prereq not in adj:
+                adj[prereq] = []
+            adj[prereq].append(course)
+
+        queue = deque([])
+
+        for course, deg in degree.items():
+            if deg == 0:
+                queue.append(course)
+
+        if not queue:
+            return False
+
+        while queue:
+            prereq = queue.popleft()
+            if prereq in adj:
+                for subject in adj[prereq]:
+                    degree[subject] -= 1
+                    if degree[subject] == 0:
+                        queue.append(subject)
+
+        for key, val in degree.items():
+            if val != 0:
+                return False
+        return True
