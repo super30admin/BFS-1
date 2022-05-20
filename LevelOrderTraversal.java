bd17233b710c7eecbbd8f2271c7cbed6b1f8ee5a
arshmohd <irfan.arshad4@gmail.com>
2022-05-21 00:19:35 +0530

    Implemented the algos for Course Schedule and Level Order Traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class LevelOrderTraversal {
+  // Time Complexity : O(N)
+  // Space Complexity : O(N)
+  // Did this code successfully run on Leetcode :
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public static List<List<Integer>> traverse(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    if (root == null)
+      return result;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      List<Integer> currentLevel = new ArrayList<>(levelSize);
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+        // add the node to the current level
+        currentLevel.add(currentNode.val);
+        // insert the children of current node in the queue
+        if (currentNode.left != null)
+          queue.offer(currentNode.left);
+        if (currentNode.right != null)
+          queue.offer(currentNode.right);
+      }
+      result.add(currentLevel);
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(3);
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(20);
+    root.right.left = new TreeNode(15);
+    root.right.right = new TreeNode(7);
+    List<List<Integer>> result = LevelOrderTraversal.traverse(root);
+    System.out.println("Level order traversal: " + result);
+  }
+}
