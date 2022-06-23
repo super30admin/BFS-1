dce9184f8e96e8f2a20ca95480d130538920b31e
Jayavani Akkiraju <jayavaniakkiraju@Jayavanis-MacBook-Pro.local>
2022-06-23 16:03:08 +0530

    BFS-1 completed

--- courseSchedule.py
+++ courseSchedule.py
@@ -1,1 +1,47 @@
+'''
+-- Passed test cases on LeetCode
+Time Complexity : O(V + E) ; Space Complexity : O(V + E)
+'''
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if (prerequisites == None or len(prerequisites) == 0):
+            return True
 
+        index = [0 for i in range(numCourses)]              
+
+        course_list = { i : [] for i in range(numCourses) }         
+
+    
+        for i in prerequisites:
+            index[i[0]] += 1
+            course_list[i[1]].append(i[0])
+
+   
+        queue = deque([])
+
+    
+        for i in range(numCourses):
+
+            if (index[i] == 0):
+                queue.append(i)
+
+        while (len(queue) > 0):
+
+            done = queue.popleft()
+
+            for i in course_list[done]:
+
+                index[i] -= 1
+
+                if (index[i] == 0):
+                    queue.append(i)
+
+      
+        for i in range(numCourses):
+
+            if (index[i] != 0):
+                return False
+
+        return True
+        
--- levelorder_traversal.py
+++ levelorder_traversal.py
@@ -1,1 +1,31 @@
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+-- Passed test cases on LeetCode
+'''
+# Recursion Approach
+class Solution:
+    #O(N) -- Time complexity where n is the number of nodes; O(N) -- Space complexity for the recursive stack to store nodes.
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        level = []
+        if not root:
+            return level
+        def getLevel(node, levels):
+            if len(level) == levels:
+                level.append([])
+            level[levels].append(node.val)
+            
+            if node.left:
+                getLevel(node.left, levels + 1)
+            if node.right:
+                getLevel(node.right, levels + 1)
+                   
+        getLevel(root, 0)
+        return level
+            
+            
+        
