fb2921681aacafc482ab87a7388a5be4209bdeec
dnyanada38 <76130280+dnyanada38@users.noreply.github.com>
2022-07-26 21:17:35 -0700

    Add python3 based solutions for problems 102_binary_tree_level_order_traversal, 207_course_schedule

---
+++ 102_binary_tree_level_order_traversal.py
@@ -0,0 +1,175 @@
+'''
+
+## Problem 102: Binary tree level order traversal.
+
+## Author: Neha Doiphode
+## Date:   07-25-2022
+
+## Description:
+    Given the root of a binary tree, return the level order traversal of its nodes' values.
+    (i.e., from left to right, level by level).
+
+## Examples:
+    Example 1:
+        Input: root = [3,9,20,null,null,15,7]
+        Output: [[3],[9,20],[15,7]]
+
+    Example 2:
+        Input: root = [1]
+        Output: [[1]]
+
+    Example 3:
+        Input: root = []
+        Output: []
+
+## Constraints:
+    The number of nodes in the tree is in the range [0, 2000].
+    -1000 <= Node.val <= 1000
+
+## Time complexity: Please refer to doc-strings of respective approaches used below to solve the problem.
+
+## Space complexity: Please refer to doc-strings of respective approaches used below to solve the problem.
+
+'''
+from queue import Queue
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def to_binary_tree(items):
+    if not items:
+        return None
+
+    # iter(object, sentinel [optional])
+    # sentinel = special character used to represent end of the sequence.
+    it = iter(items)
+    root = TreeNode(next(it))
+    q = [root]
+    for node in q:
+        val = next(it, None)
+        if val is not None:
+            node.left = TreeNode(val)
+            q.append(node.left)
+        val = next(it, None)
+        if val is not None:
+            node.right = TreeNode(val)
+            q.append(node.right)
+    return root
+
+def get_input():
+    print("Enter nodes of tree with spaces at height h, from left to right.")
+    print("Input null if no left or right child to a node at height h.")
+    inp_list = input()
+    inp_list = inp_list.split()
+    l = []
+    for value in inp_list:
+        if value != "null":
+            l.append(int(value))
+        else:
+            l.append(None)
+
+    root = to_binary_tree(l)
+    if root.val == None:
+        l = []
+        root = None
+    return l, root
+
+class Solution:
+    # Used in the DFS based approach
+    result = []
+    def levelOrder_bfs(self, root: Optional[TreeNode]) -> List[List[int]]:
+        '''
+        Approach: Breadth First Search
+        Time complexity : O(n), where n is number of nodes in the tree.
+                                We traverse each node once.
+        Space complexity: O(n), At a time auxiliary queue will have number of nodes at level 'l'.
+                                Max number of nodes will be present at the last level if all each node on the previous level
+                                has left and right child both.
+                                For example,
+                                if a tree has 4 levels, and each node has left and right child both,
+                                level 0 = 1 node(root)(2 ^ 0)
+                                level 1 = 2 (2 ^ 1)
+                                level 3 = 4 (2 ^ 2)
+                                level 4 = 8 (2 ^ 3)
+
+                                Total number of nodes in the tree = 1 + 2 + 4 + 8 = 15
+                                Nodes at the last level           = 8 ~= n/2
+
+                                So, at a time, at the max n/2 nodes will be present in the queue.
+                                Asymptotically, we can say that space complexity is O(n/2) ~= O(n).
+        '''
+        if root == None:
+            return []
+
+        answer = []
+        q = Queue()
+        q.put(root)
+
+        while not q.empty():
+            size = q.qsize()
+            temp = []
+            while size:
+                root = q.get()
+                temp.append(root.val)
+
+                if root.left:
+                    q.put(root.left)
+
+                if root.right:
+                    q.put(root.right)
+
+                size -= 1
+            if len(temp) != 0:
+                answer.append(temp)
+        return answer
+
+    def dfs(self, root: TreeNode, level: int) -> None:
+        if root == None:
+            return
+
+        if level == len(self.result):
+            temp = []
+            self.result.append(temp)
+
+        self.result[level].append(root.val)
+        self.dfs(root.left, level + 1)
+        self.dfs(root.right, level + 1)
+
+    def levelOrder_dfs(self, root: Optional[TreeNode]) -> List[List[int]]:
+        '''
+        Approach: Depth First Search
+                  Approach makes use of pre-order tree traversal.
+                  As we reach a certain level/height h of the tree, we just initiate a new list.
+                  During traversal, when we reach a particular node at level l,
+                  we just append value of the node to the list reated for that level.
+
+                  We can choose in-order traversal as well,
+                  but the approach needs us to count levels present in the tree first,
+                  to create number of blank lists. So, add the overhead of counting levels first
+
+        Time complexity : O(n), where n is number of nodes in the tree.
+                                We traverse each node once.
+        Space complexity: O(h), where h is the height of the tree.
+        '''
+        if root == None:
+            return []
+
+        level = 0
+        self.result = []
+        self.dfs(root, level)
+        return self.result
+
+
+# Driver code
+# Driver code
+solution = Solution()
+l, root = get_input()
+print(f"Input Tree: {l}", end = "")
+print()
+print(f"Approach 1: With BFS: Level order traversal: {solution.levelOrder_bfs(root)}")
+print(f"Approach 2: With DFS: Level order traversal: {solution.levelOrder_dfs(root)}")
---
+++ 207_course_schedule.py
@@ -0,0 +1,154 @@
+'''
+## Problem 207: Course schedule.
+
+## Author: Neha Doiphode
+## Date:   07-25-2022
+
+## Description:
+    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+    You are given an array prerequisites where prerequisites[i] = [ai, bi],
+    indicates that you must take course bi first if you want to take course ai.
+
+    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    Return true if you can finish all courses. Otherwise, return false.
+
+## Examples:
+    Example 1:
+        Input: numCourses = 2, prerequisites = [[1,0]]
+        Output: true
+        Explanation: There are a total of 2 courses to take.
+                     To take course 1 you should have finished course 0. So it is possible.
+
+    Example 2:
+        Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+        Output: false
+        Explanation: There are a total of 2 courses to take.
+                     To take course 1 you should have finished course 0,
+                     and to take course 0 you should also have finished course 1. So it is impossible.
+
+    Example 3:
+        Input: numCourses = 5, prerequisites = [[1,4],[2,4],[3,1],[3,2]]
+        Output: True
+
+## Constraints:
+    1 <= numCourses <= 2000
+    0 <= prerequisites.length <= 5000
+    prerequisites[i].length == 2
+    0 <= ai, bi < numCourses
+    All the pairs prerequisites[i] are unique.
+
+## Notes:
+    Graph concepts:
+        * Types of Graph Representations:
+            - Adjecency matrix
+            - Adjecency list
+
+        * In-degrees array = array with vertices as indices and values as number of incoming edges to the node.
+                    0
+                /       \
+               2         3
+                  \   /
+               |    1    |
+               4    -    5
+
+               Input array: (in, out): [[2, 0][3, 0][1, 2][1, 3][4, 2][5, 4][5, 3]]
+
+               In-degrees:Indices                 :[0, 1, 2, 3, 4, 5] (Vertices)
+                          Number of incoming edges:[0, 2, 1, 1, 1, 2] (number of dependencies on the corresponding vertex/index)
+
+        * Adjecency list:   0: [2, 3]
+                            2: [1, 4]
+                            3: [1, 5]
+                            4: [5]
+                            1: []
+                            5: []
+
+        * Topological sort:
+            1. We start from an independent node.
+            2. Use in-degrees array, a queue and adjecency list.
+            3. Using a queue, try to clear out in-degrees array.
+               If we are able to clear the array, there is no cycle present in the graph.
+               If we are NOT able to clear in-degrees array, there is cycle existing in the graph.
+               Nodes are added to the queue based on the criteria that,
+               if all dependencies for a vertex/in-degree array for a node is cleared.
+
+            Definition: Topological sort or topological ordering of a directed graph is a linear ordering of its vertices such that,
+                        for every directed edge uv from vertex u to vertex v, u comes before v in the ordering.
+
+
+## Time complexity: O(V + E),   where n is number of courses.
+                                To build in-degrees array, we go over all dependencies.
+                                Later, For each vertex, we don't iterate upon all edges.
+                                We only iterate over only few edges.
+                                Hence the time complexity is V + E and NOT V * E.
+
+                                If every vertex has edge over every other vertex, at a time,
+                                we iterate over ONLY corresponding edges and not all edges.
+
+## Space complexity: O(V + E),  For In-degrees array: Number of courses
+                                For Adjecency list  : Number of courses
+                                For Queue           : Number of courses
+                                We store values based on number of edges per vertex.
+'''
+
+from queue import Queue
+from typing import List, Optional
+
+def get_input():
+    print("Enter total number of courses: ", end = "")
+    numCourses = int(input())
+    print("Enter total number of prerequisite entries: ", end = "")
+    numPreReqs = int(input())
+    prerequisites = []
+    print("Enter pairs in the format: Course number <space> Pre-requisite course number")
+    for preReq in range(numPreReqs):
+        print(f"Pre-requisite requirement {preReq + 1}: ", end = "")
+        l = input()
+        l = [int(val) for val in l.split()]
+        prerequisites.append(l)
+    return numCourses, prerequisites
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if numCourses == 0:
+            return True
+
+        # step 1:
+        # Create adjecency list and indegrees array
+        courses = {}
+        for num in range(numCourses):
+            courses[num] = []
+
+        # step 2: Fill in adjecency list and indegrees array
+        indegrees = [0] * numCourses
+        for pair in prerequisites:
+            if not pair[1] in courses:
+                courses[pair[1]] = []
+            courses[pair[1]].append(pair[0])
+            indegrees[pair[0]] += 1
+
+        # Step 3: Clear in-degrees array based on pre-requisites
+        #         with the help of adjecency list and queue/BFS
+        q = Queue()
+        for index, node in enumerate(indegrees):
+            if node == 0 :
+                q.put(index)
+
+        while not q.empty():
+            node = q.get()
+            depedent_nodes = courses[node]
+            if len(depedent_nodes) != 0:
+                for node in depedent_nodes:
+                    indegrees[node] -= 1
+                    if indegrees[node] == 0:
+                        q.put(node)
+
+        # Cyclic dependencies don't exist if all dependencies in indegrees are cleared out.
+        return all(val == 0 for val in indegrees)
+
+# Driver code
+solution = Solution()
+numCourses, prerequisites = get_input()
+print(f"Input: Total number of courses to be completed: {numCourses}")
+print(f"Input: Pre-requisites(Course number , Pre-requisite course number): {prerequisites}")
+print(f"Is it possible to complete all courses?: {solution.canFinish(numCourses, prerequisites)}")
