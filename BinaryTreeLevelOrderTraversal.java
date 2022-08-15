ee427792c3b49fa93d7c5364f770b4d02b032700
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-08-15 12:39:58 -0700

    Binary Tree Level Order Traversal using BFS

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,62 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+
+//102. Binary Tree Level Order Traversal (Medium) - https://leetcode.com/problems/binary-tree-level-order-traversal/
+class Solution {
+    
+    List<List<Integer>> result;
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+    	// Time Complexity : O(n) where n = number of elements of the tree, Space Complexity : O(n) where n = width of the tree
+//         List<List<Integer>> result = new ArrayList<>();
+        
+//         if (root == null) return result;
+        
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             List<Integer> temp = new ArrayList<>();
+            
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode curr = queue.poll();
+//                 temp.add(curr.val);
+                
+//                 if (curr.left != null) queue.add(curr.left);
+//                 if (curr.right != null) queue.add(curr.right);
+//             }
+            
+//             result.add(temp);
+//         }
+        
+//         return result;
+    	
+    	// Time Complexity: O(n), Space Complexity : O(h) where h = height of the tree
+        result = new ArrayList<>();
+        
+        if (root == null) return result;
+        
+        dfs(root, 0);
+        return result;
+    }
+    
+    private void dfs(TreeNode root, int level) {
+        // base
+        if (root == null) return;
+        
+        // logic
+        if (level == result.size()) {
+            result.add(new ArrayList<>());
+        }
+        
+        result.get(level).add(root.val);
+        
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+}
