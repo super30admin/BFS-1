2193dc33f7804bc8072e8249e18d14d29ad03b2c
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-08-15 12:57:07 -0700

    2 Stacks solution

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -37,26 +37,59 @@ class Solution {
 //         return result;
     	
     	// Time Complexity: O(n), Space Complexity : O(h) where h = height of the tree
-        result = new ArrayList<>();
+//        result = new ArrayList<>();
+//        
+//        if (root == null) return result;
+//        
+//        dfs(root, 0);
+//        return result;
+//    }
+//    
+//    private void dfs(TreeNode root, int level) {
+//        // base
+//        if (root == null) return;
+//        
+//        // logic
+//        if (level == result.size()) {
+//            result.add(new ArrayList<>());
+//        }
+//        
+//        result.get(level).add(root.val);
+//        
+//        dfs(root.left, level + 1);
+//        dfs(root.right, level + 1);
+    	
+    	// Time Complexity : O(n) where n = number of elements of the tree, Space Complexity : O(n) where n = width of the tree
+        List<List<Integer>> result = new ArrayList<>();
         
         if (root == null) return result;
         
-        dfs(root, 0);
-        return result;
-    }
-    
-    private void dfs(TreeNode root, int level) {
-        // base
-        if (root == null) return;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> levelStack = new Stack<>();
         
-        // logic
-        if (level == result.size()) {
-            result.add(new ArrayList<>());
+        nodeStack.push(root);
+        levelStack.push(0);
+        
+        while (!nodeStack.isEmpty()) {
+            TreeNode curr = nodeStack.pop();
+            int level = levelStack.pop();
+            
+            if (level == result.size()) {
+                result.add(new ArrayList<>());
+            }
+            
+            result.get(level).add(curr.val);
+            
+            if (curr.right != null) {
+                nodeStack.push(curr.right);
+                levelStack.push(level + 1);
+            }
+            if (curr.left != null) {
+                nodeStack.push(curr.left);
+                levelStack.push(level + 1);
+            }
         }
         
-        result.get(level).add(root.val);
-        
-        dfs(root.left, level + 1);
-        dfs(root.right, level + 1);
+        return result;
     }
 }
