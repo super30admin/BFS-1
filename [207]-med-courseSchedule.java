948441fe6bb3b23a7f4ec0b26a4610cbbc58408b
parag28 <parag@aoguest-10-6-13-81.syr.edu>
2022-09-16 18:41:25 -0400

    Completed bfs-1

---
+++ [207]-med-courseSchedule.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] edges) {
+        int indegrees[]=new int[numCourses];
+        HashMap <Integer, List<Integer>> map= new HashMap<>();
+
+        //adding to indegreees array
+        for(int edge[]:edges){
+            indegrees[edge[0]]++;
+
+            if(!map.containsKey(edge[1]))
+                map.put(edge[1], new ArrayList<>());
+
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        //bfs
+        Queue <Integer> q= new LinkedList<>();
+        int count=0;
+
+        for(int i=0;i<numCourses;i++){
+            if(indegrees[i]==0) {
+                q.add(i);
+                count++;
+            }
+        }
+
+        if(count==numCourses) return true;  //imp
+
+        while(!q.isEmpty()){
+            int curr= q.poll();
+            List<Integer> children= map.get(curr);
+
+            if(children!=null){
+                for(int child:children){
+                    indegrees[child]--;
+                    if(indegrees[child]==0){
+                        q.add(child);
+                        count++;
+                        if(count==numCourses) return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+
+    }
+}
