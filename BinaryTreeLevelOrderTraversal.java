d9ab82562f8f491fc33e520bd2cf1a97aa356f4a
Jackson Raj A <jacksonraj1095@gmail.com>
2023-01-18 23:01:57 +0530

    Completed Implmentation of BFS - 1

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,73 @@
+// Time Complexity  : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+// Time Complexity  : O(N)
+// Space Complexity : O(H)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class BinaryTreeLevelOrderTraversalDFS {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(root, 0, result);
+        return result;
+    }
+
+    private void dfs(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+        if (result.size() == level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        dfs(root.left, level + 1, result);
+        dfs(root.right, level + 1, result);
+    }
+}
+
+class BinaryTreeLevelOrderTraversalBFS {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> subList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+}
