ad7b4fab876613821b95052a4973d66aaf472c2b
pradhatrivemula1 <pradhatri.vemula1@gmail.com>
2023-02-26 19:48:14 -0800

    commited

---
+++ courseSchedule.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int [] arr = new int[numCourses];
+        HashMap<Integer, List<Integer>> map= new HashMap<>();
+        Queue<Integer> q= new LinkedList<>();
+        int count =0;
+        for(int[]p: prerequisites){
+            arr[p[0]]++;
+            if(map.containsKey(p[1])) map.get(p[1]).add(p[0]);
+            else{
+                List<Integer> l= new ArrayList<Integer>();
+                l.add(p[0]); 
+                map.put(p[1],l);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                q.add(i);
+                count++;
+            }
+        }
+        if(q.size()==0) return false;
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            if(map.containsKey(curr)){
+               List <Integer> l=  map.get(curr);
+               for(int a:l){
+                   arr[a]--;
+                   if(arr[a]==0){ q.add(a);
+                   count++;}
+               }
+            }
+        }
+        return (count==numCourses);
+    }
+}
