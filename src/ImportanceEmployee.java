2254d570967d5bfbb567f371092954e604af37b1
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-04-13 11:18:35 -0700

    Update ImportanceEmployee.java

--- src/ImportanceEmployee.java
+++ src/ImportanceEmployee.java
@@ -13,10 +13,13 @@ class Employee {
 }
 
 public class ImportanceEmployee {
+    
+    // ******************** BFS Approach ********************
+    
     public int getImportance(List<Employee> employees, int id) {
         Map<Integer, Employee> map = new HashMap<>();
         for(Employee e : employees) {
-            map.put(e.id, e);                              // adding all ids in map
+            map.put(e.id, e);                              // adding all ids and employee objects in map
         }
 
         int totalImp = 0;
@@ -26,7 +29,7 @@ public class ImportanceEmployee {
         q.add(map.get(id));
 
         while(!q.isEmpty()) {
-            Employee e = q.remove();
+            Employee e = q.remove();                       // processing current employee
             totalImp += e.importance;                      // adding importance in result
             for(int subId : e.subordinates) {
                 q.add(map.get(subId));                     // adding children in queue
@@ -35,4 +38,34 @@ public class ImportanceEmployee {
 
         return totalImp;
     }
+    
+//     // ******************** DFS Approach ********************
+    
+//     Map<Integer, Employee> map;
+//     int totalImp = 0;
+
+//     public int getImportance(List<Employee> employees, int id) {
+
+//         map = new HashMap<>();
+
+//         for(Employee e : employees) {                                                   // maintaining map for employee id, employee object
+//             map.put(e.id, e);
+//         }
+
+//         dfs(id);
+
+//         return totalImp;
+
+//     }
+
+//     private void dfs(int id) {
+
+//         Employee e = map.get(id);                                                       // getting employee object from current employee id
+//         totalImp += e.importance;                                                       // adding importance of the current employee
+
+//         for(int subId : e.subordinates) {                                               // calling dfs on all subordinates
+//             dfs(subId);
+//         }
+
+//     }
 }
