a68c28361ecd56135608fcadd7e4c089a925bdad
amreenmahenaz <113376911+amreenmahenaz@users.noreply.github.com>
2023-06-21 17:01:17 -0700

    Update and rename coursecalendar.py to courseschedule.py

--- courseschedule.py
+++ courseschedule.py
@@ -1,5 +1,5 @@
-# // Time Complexity :O(N)
-# // Space Complexity :O(n)
+# // Time Complexity :O(n+m)
+# // Space Complexity :O(n+m)
 # // Did this code successfully run on Leetcode :Yes
 # // Any problem you faced while coding this :no
 class Solution:
