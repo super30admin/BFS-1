f0dea2ecd0d8bfc630f71d8bc429ebd621f153a9
sanahpathan <134433810+sanahpathan@users.noreply.github.com>
2023-08-25 17:17:01 -0700

    Create CourseSchedule.java

---
+++ CourseSchedule.java
@@ -0,0 +1,47 @@
+import java.util.*;
+// TC : O(m + n) 
+// SC : O(m + n) 
+//where m is the number of courses and n is the number of prerequi
+public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length==0)
+            return true;
+        HashMap<Integer, List<Integer>> courseMapping=new HashMap<>(); //hashmap to add the course mapping
+        int[] inDegree=new int[numCourses]; //to maintain the current dependency of courses
+
+        for(int[] pr:prerequisites){
+            //pr[0] is dependent course
+            //pr[1] is independent course
+            inDegree[pr[0]]++;
+            if(!courseMapping.containsKey(pr[1])){
+                courseMapping.put(pr[1], new ArrayList<>());
+            }
+            courseMapping.get(pr[1]).add(pr[0]);
+        }
+        Queue<Integer> independentQue=new LinkedList<>();
+        int count=0;
+        for(int i=0;i<numCourses;i++){
+            if(inDegree[i]==0){
+                independentQue.add(i);
+                count++;
+            }
+        }
+        while(!independentQue.isEmpty()){
+            int curr=independentQue.poll();
+            List<Integer> children=courseMapping.get(curr);
+            if(children!=null){
+                for(int child:children){
+                    inDegree[child]--;
+                    if(inDegree[child]==0){
+                        independentQue.add(child);
+                        count++;
+                        if(count==numCourses){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
