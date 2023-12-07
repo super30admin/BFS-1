39cc083a0c5fcead9262e15527ea7f45a039df35
Smriti0311 <smritigrao@gmail.com>
2023-12-07 17:43:32 +0530

    course schedule dfs java

--- prob2.java
+++ prob2.java
@@ -15,7 +15,7 @@ public class prob2 {
             }
             adjList.get(edge[1]).add(edge[0]);
         }
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<Integer>();
         int count = 0;
         for(int i = 0; i < indegrees.length; i++){
             if(indegrees[i] == 0){
@@ -39,3 +39,49 @@ public class prob2 {
         return count == numCourses;
     }
 }
+
+// DFS solution, NOT Intuitive
+/*class Solution {
+    boolean[] path;
+    boolean[] visited;
+    Map<Integer, List<Integer>> map;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses < 1 || prerequisites == null) return false;
+        map = new HashMap<>();
+        path = new boolean[numCourses];
+        visited = new boolean[numCourses];
+        for(int[] edge: prerequisites){
+            int in = edge[0]; int out = edge[1];
+            if(!map.containsKey(out)){
+                map.put(out, new ArrayList<>());
+            }
+            map.get(out).add(in);
+        }
+        for(int i = 0; i < numCourses; i++){
+            if(!visited[i] && hasCycle(i)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(int course){
+        //base
+        if(path[course]) return true;
+        if(visited[course]) return false;
+
+
+        //logic
+        visited[course] = true;
+        //action
+        path[course] = true;
+        List<Integer> children = map.get(course);
+        if(children != null){
+            for(int child: children){
+                if(hasCycle(child)) return true;
+            }
+        }
+        // backtrack
+        path[course] = false;
+        return false; 
+    }
+} */
