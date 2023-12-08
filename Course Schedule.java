a4bde1c7d394a8e75ebb76d921503f8644ae53bc
Prakhar Srivastava <prakharsrivastava@Prakhars-Air.attlocal.net>
2023-12-07 17:14:39 -0800

    Completed BFS-1

---
+++ Course Schedule.java
@@ -0,0 +1,56 @@
+// Time Complexity :O(N+M)  N-number of Courses, M- number of  prerequisites
+// Space Complexity :O(N+M)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       List<List<Integer>> adj = new ArrayList<>();
+       for(int i=0; i<numCourses; i++)
+       adj.add(new ArrayList<>());
+
+       for(int i=0; i< prerequisites.length; i++)
+       {
+           adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+       }
+
+       int []visited = new int[numCourses];
+       for(int i=0; i<numCourses; i++)
+       {
+           if(visited[i]==0)
+           {
+               if(isCyclic(adj,visited,i))
+               {
+                   return false;
+               }
+           }
+
+       }
+       return true;
+
+       
+
+    }
+    private boolean isCyclic(List<List<Integer>> adj,int []visited, int curr)
+    {
+        if(visited[curr]==2)
+        return true;
+
+        visited[curr]=2;
+        for(int i=0; i<adj.get(curr).size(); i++)
+        {
+            if(visited[adj.get(curr).get(i)]!=1)
+            {
+                if(isCyclic(adj,visited, adj.get(curr).get(i))){
+                    return true;
+                }
+            }
+
+        }
+        visited[curr]=1;
+        return false;
+    }
+}
