7d76d9c14d1dce7ee639c1f26e0b08dac68106eb
Shreya Agrawal <84276060+ag-sh1996@users.noreply.github.com>
2024-01-17 23:04:29 -0600

    Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,4 +1,4 @@
-// Time Complexity :O(V * E)
+// Time Complexity :O(V + E)
 // Space Complexity :O(V + E)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
