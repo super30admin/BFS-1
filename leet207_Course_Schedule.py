5b530ff91ca321d52034d27801c5868fdb6a952b
SathwikReddyGK <sathwikreddy.gk@gmail.com>
2024-02-11 01:55:03 -0600

    S30 Assignment, Course Schedule with hasCycle completed

--- leet207_Course_Schedule.py
+++ leet207_Course_Schedule.py
@@ -1,14 +1,20 @@
 # Solution
 
-# // Time Complexity : O(V+E) since we will be removing all the edges one by one to see if all the edges can be removed and
+# // Time Complexity : O(V+E) even in check cycle approach, since we do not process the same vertex again once it is marked in DP
+#                      Incase DP is not maintained, then it will be V^V
+#                      O(V+E) since we will be removing all the edges one by one to see if all the edges can be removed and
 #                      since we do it by going for each vertex once, so we need to include vertex count as well.
-# // Space Complexity : O(2V+2E), since dictionary will have all the edges and vertices, indegree will have size equal to number
+# // Space Complexity : O(V) since we keep an array for all the vertices to mark in path and an array for all vertices for DP
+#                       O(2V+2E), since dictionary will have all the edges and vertices, indegree will have size equal to number
 #                       of vertices and queue size can go upto numbmer of edges in worst case scenario
 # // Did this code successfully run on Leetcode : Yes
 # // Any problem you faced while coding this : None
 
 
 # // Your code here along with comments explaining your approach
+# Check Cycle Approach: We can keep array to make a note of visited nodes in each cycle and one array to keep overall if we have
+# reached node which does not have any other edge to be processed. Based on these two, we can decide if there is a cycle or not
+# by checking if any vertex already has marked "True" when we are going to that vertex as we are performing DFS
 # Approach is to come up with indegree for all the courses. If as we take courses, indegrees of all courses reduce to zero
 # that means we can take all courses. Create a list to hold indegree of each course. Create a adjacency list(using dictionary) for the map/graph which
 # keeps track of all the destinations node for each source node, with source node as key of dictonary
@@ -19,43 +25,75 @@
 
 from collections import deque
 def canFinish(numCourses, prerequisites):
-        map = {}
-        inDegree = [0 for _ in range(0,numCourses)]
-        verticesQueue = deque()
-        count = 0
-
-        if not prerequisites:
-            return True
-
-        for prerequisite in prerequisites:
-            out = prerequisite[1]
-            to = prerequisite[0]
-            if out not in map:
-                map[out] = []
-            map[out].append(to)
-            inDegree[to] += 1
-        
-        for i in range(0,numCourses):
-            if inDegree[i] == 0:
-                verticesQueue.append(i)
-        
-        while verticesQueue:
-            count += 1
-            vertex = verticesQueue.popleft()
-            if vertex in map:
-                for out in map[vertex]:
-                    inDegree[out] -= 1
-                    if inDegree[out] == 0:
-                        verticesQueue.append(out)
-                
-        if count == numCourses:
-            return True
+    map = {}
+    dp = [False for _ in range(numCourses)]
+    pathVisit = [False for _ in range(numCourses)]
+    for prerequisite in prerequisites:
+        if prerequisite[1] in map:
+            map[prerequisite[1]].append(prerequisite[0])
         else:
-            return False
+            map[prerequisite[1]] = [prerequisite[0]]
+    
+    for items in map.items():
+        if not dp[items[0]]:
+            if hasCycle(items[0],map,pathVisit,dp):
+                return False
+    return True
+
+def hasCycle(vertex,map,pathVisit,dp):
+    if dp[vertex] == True:
+        return False
+    
+    if pathVisit[vertex] == True:
+        return True
+    
+    pathVisit[vertex] = True
+
+    if vertex in map:
+        for vertices in map[vertex]:
+            if hasCycle(vertices,map,pathVisit,dp):
+                return True
+    
+    pathVisit[vertex] = False
+    dp[vertex] = True
+
+        # map = {}
+        # inDegree = [0 for _ in range(0,numCourses)]
+        # verticesQueue = deque()
+        # count = 0
+
+        # if not prerequisites:
+        #     return True
+
+        # for prerequisite in prerequisites:
+        #     out = prerequisite[1]
+        #     to = prerequisite[0]
+        #     if out not in map:
+        #         map[out] = []
+        #     map[out].append(to)
+        #     inDegree[to] += 1
+        
+        # for i in range(0,numCourses):
+        #     if inDegree[i] == 0:
+        #         verticesQueue.append(i)
+        
+        # while verticesQueue:
+        #     count += 1
+        #     vertex = verticesQueue.popleft()
+        #     if vertex in map:
+        #         for out in map[vertex]:
+        #             inDegree[out] -= 1
+        #             if inDegree[out] == 0:
+        #                 verticesQueue.append(out)
+                
+        # if count == numCourses:
+        #     return True
+        # else:
+        #     return False
 
 if __name__ == "__main__":
-    numCourses = 2
-    prerequisites = [[1,0]]
     # numCourses = 2
-    # prerequisites = [[1,0],[0,1]]
+    # prerequisites = [[1,0]]
+    numCourses = 2
+    prerequisites = [[1,0],[0,1]]
     print(canFinish(numCourses,prerequisites))
