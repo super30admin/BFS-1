ee0f7a47470ee8e25894ec40fae85ae75b611b54
Shefali Singh <shefali.singh@gomotive.com>
2024-02-12 14:59:18 -0800

    done BFS-1

---
+++ level_order_traversal.java
@@ -0,0 +1,45 @@
+//Time - O(n)
+//Space - O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode node = q.poll();
+                temp.add(node.val);
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+        
+    }
+}
+
