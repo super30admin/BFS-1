e06bf1bdaa44875e543bda375315d2c5cd617d29
Riyazmd007Riyazmd007Riyazmd007 <rm2883@nau.edu>
2024-02-21 13:55:34 -0700

    BFS 1

---
+++ BT_level_order.java
@@ -0,0 +1,47 @@
+//TC: O(n)
+//SC: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root==null)
+        return result;
+       q.add(root);
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i<size;i++)
+            {
+               TreeNode curr=q.poll();
+               temp.add(curr.val);
+               if(curr.left!=null)
+               {
+                   q.add(curr.left);
+               }
+               if(curr.right!=null)
+               {
+                   q.add(curr.right);
+               }
+            }
+            result.add(temp);
+        }
+return result;
+        
+    }
+}
